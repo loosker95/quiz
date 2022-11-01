15fd0a2297f67f3f285ac2f22bbd792b3957f474
Fabien <loosker95@gmail.com>
2022-11-01 15:58:11 +0700

    Create API

--- models/user.model.js
+++ models/user.model.js
@@ -1,51 +1,43 @@
-'use strict';
+const { DataTypes } = require("sequelize");
+const sequelize = require("./connect");
 
-const Sequelize = require('sequelize');
-const db = require('./connect')
+const users = sequelize().define("users", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    isAlphanumeric: true,
+  },
+  fullname: {
+    type: DataTypes.STRING,
+    isAlphanumeric: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    isEmail: true,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(64),
+    validate: {
+      is: /^[0-9a-f]{64}$/i,
+    },
+    min: 8
+  },
+  avatar: DataTypes.STRING,
+  role: {
+    type: DataTypes.STRING,
+    isIn: [['admin', 'basic']]
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  }
+});
 
-const seq = (sequelize, DataTypes) => {
-  const users = sequelize.define('users', {
-    id: DataTypes.INTEGER,
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      isAlphanumeric: true,
-    },
-    fullname: {
-      type: DataTypes.STRING,
-      isAlphanumeric: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      isEmail: true,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING(64),
-      validate: {
-        is: /^[0-9a-f]{64}$/i,
-      },
-      min: 8
-    },
-    avatar: DataTypes.STRING,
-    role: {
-      type: DataTypes.STRING,
-      isIn: [['admin', 'basic']]
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    }
-  }, {});
-  users.associate = function(models) {
-    // associations can be defined here
-  };
-  return users;
-};
-
+module.exports = users;

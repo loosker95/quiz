21c8fa263308ad7f0468f123fe48f81da2d70906
Fabien <loosker95@gmail.com>
2022-11-02 22:39:56 +0700

    Update route

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,15 +1,46 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user.controller')
+const resultController = require('../../controllers/result.controller')
+const questionController = require('../../controllers/question.controller')
+const answerController = require('../../controllers/answer.controller')
 
-router.post('/api/v1/users', userController.addUser)
-router.get('/api/v1/users', userController.getAllUsers)
-router.get('/api/v1/:id', userController.findUser)
-router.patch('/api/v1/:id', userController.updateUser)
-router.delete('/api/v1/:id', userController.deleteUser)
 
-router.post('/api/v1/login', userController.loginUser)
-router.post('/api/v1/register', userController.registerUser)
+// Users
+router.post('/users', userController.addUser)
+router.get('/users', userController.getAllUsers)
+router.get('/users/:id', userController.findUser)
+router.patch('/users/:id', userController.updateUser)
+router.delete('/users/:id', userController.deleteUser)
+
+router.post('/login', userController.loginUser)
+router.post('/register', userController.registerUser)
+
+
+// Results
+router.post('/results', resultController.addResult)
+router.get('/results', resultController.getAllResult)
+router.get('/results/:id', resultController.findResult)
+router.patch('/results/:id', resultController.updateResult)
+router.delete('/results/:id', resultController.deleteResult)
+
+router.get('/view/:id', resultController.viewResult)
+
+
+// // Questions
+router.post('/questions', questionController.addQuestion)
+router.get('/questions', questionController.allQuestions)
+router.get('/questions/:id', questionController.findQuestion)
+router.patch('/questions/:id', questionController.updateQuestion)
+router.delete('/questions/:id', questionController.deleteQuestion)
+
+
+// // Answers
+router.post('/answers', answerController.addAnswer);
+router.get('/answers', answerController.getAllAnswers);
+router.get('/answers/:id', answerController.findAnswer);
+router.patch('/answers/:id', answerController.updateAnswer);
+router.delete('/answers/:id', answerController.deleteAnswer);
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const connectDb = require('./models/connect')
 const bodyParser = require('body-parser')
-
-
-const userRoute = require('./routes/v1/user.route')
-const questionRoute = require('./routes/v1/question.route')
-const answerRoute = require('./routes/v1/answer.route')
-const resultRoute = require('./routes/v1/result.route')
+const routes = require('./routes/v1/user.route')
 
 dotenv.config({path: './config.env'})
 port = process.env.PORT || 1010
@@ -19,11 +14,7 @@ connectDb();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/v1', userRoute)
-app.use('/v1/questions', questionRoute)
-app.use('/v1/answers', answerRoute)
-app.use('/v1/results', resultRoute)
-app.use('/v1/view', resultRoute)
+app.use('/api/v1', routes)
 
 app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:${port}/v1`)
